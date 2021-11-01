3a334678a77b4cec737f9c0709344bb8f1f01184
ahmadbilalse <ahmadbilal.se@gmail.com>
2021-11-01 16:11:00 +0700

    add: loader animation

--- components/SearchForm/SearchForm.tsx
+++ components/SearchForm/SearchForm.tsx
@@ -1,13 +1,35 @@
-import React from 'react';
-import styles from './SearchForm.module.scss';
-import { BsSearch } from 'react-icons/bs';
+import React, { useState } from "react";
+import styles from "./SearchForm.module.scss";
+import { BsSearch } from "react-icons/bs";
+import Loader from "../Loader/Loader";
 
 export default function SearchForm() {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleClick = () => {
+    setIsLoading(true);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  };
+
   return (
     <div className={styles.container}>
       <BsSearch className={styles.icon} />
-      <input placeholder="Search GitHub username..." className={styles.input} type="text" />
-      <button className={styles.button}>Search</button>
+      <input
+        placeholder="Search GitHub username..."
+        className={styles.input}
+        type="text"
+      />
+      {isLoading ? (
+        <div className={styles.loader}>
+          <Loader />
+        </div>
+      ) : (
+        <button onClick={handleClick} className={styles.button}>
+          Search
+        </button>
+      )}
     </div>
-  )
+  );
 }

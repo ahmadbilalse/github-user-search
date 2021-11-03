70901523d54f6c0c4367d85a5cdcc1b131dddb5d
ahmadbilalse <ahmadbilal.se@gmail.com>
2021-11-03 07:37:37 +0700

    add: clear input button

--- components/SearchForm/SearchForm.tsx
+++ components/SearchForm/SearchForm.tsx
@@ -1,4 +1,4 @@
-import React, { SyntheticEvent, useEffect, useState } from "react";
+import React, { SyntheticEvent, useEffect, useRef, useState } from "react";
 import styles from "./SearchForm.module.scss";
 import { BsSearch } from "react-icons/bs";
 import { useLazyQuery } from "@apollo/client";
@@ -9,13 +9,21 @@ export default function SearchForm() {
   const [input, setInput] = useState("");
   const [getData, { loading }] = useLazyQuery(GITHUB_USER_DATA);
   const gSetInput = useStore((state) => state.setInput);
+  const inputEl = useRef<HTMLInputElement>(null);
 
-  const handleClick = (e: SyntheticEvent) => {
+  const handleSubmit = (e: SyntheticEvent) => {
     e.preventDefault();
     getData({
       variables: { login: input },
     });
     gSetInput(input);
+  };
+
+  const handleClear = (e: SyntheticEvent) => {
+    setInput("");
+    if (inputEl.current) {
+      inputEl.current.focus();
+    }
   };
 
   const handleChange = (e: SyntheticEvent) => {
@@ -26,6 +34,7 @@ export default function SearchForm() {
     <form className={styles.container}>
       <BsSearch className={styles.icon} />
       <input
+        ref={inputEl}
         onChange={handleChange}
         value={input}
         placeholder="Search GitHub username..."
@@ -33,9 +42,16 @@ export default function SearchForm() {
         type="text"
       />
       {loading ? null : (
-        <button onClick={handleClick} className={styles.button}>
-          Search
-        </button>
+        <>
+          {input ? (
+            <div onClick={handleClear} className={styles.clearButton}>
+              &#215;
+            </div>
+          ) : null}
+          <button onClick={handleSubmit} className={styles.button}>
+            Search
+          </button>
+        </>
       )}
     </form>
   );
